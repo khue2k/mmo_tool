96647584b54f4b1617bee34a8900414903a249e1
khue29 <songkhecole@gmail.com>
2024-08-06 01:15:02 +0700

    update

---
+++ timeFarm/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+VAR=$1
+read VAR
+echo "My value is $VAR"
+
--- timeFarm/timeFarm.sh
+++ timeFarm/timeFarm.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
+
 DIRECTORY="/home/ec2-user/timeFarm"
 FILE="time.js"
+OUTPUT_FILE="/home/ec2-user/timeFarm/nohup.out"
+INPUT=$1
+
 if [ -d "$DIRECTORY" ]; then
   # Di chuyển vào thư mục chứa file JavaScript
   cd "$DIRECTORY" || { echo "Không thể di chuyển vào thư mục $DIRECTORY"; exit 1; }
-  
+
   # Kiểm tra xem file JavaScript có tồn tại không
   if [ -f "$FILE" ]; then
-    # Chạy file JavaScript bằng Node.js
-    node "$FILE"
+    # Chạy file JavaScript bằng Node.js với dữ liệu đầu vào từ biến INPUT, ghi output vào nohup.out
+    echo "$INPUT" | nohup node "$FILE" > "$OUTPUT_FILE" 2>&1 &
+    echo "Chạy file JavaScript trong nền, output được ghi vào $OUTPUT_FILE"
   else
     echo "File $FILE không tồn tại trong thư mục $DIRECTORY"
     exit 1
