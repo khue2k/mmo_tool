74457ae7e0833bd5bcd7951c3b82ec27746ffece
khue29 <songkhecole@gmail.com>
2024-08-06 22:23:47 +0700

    update

---
+++ bananav2/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIRECTORY="/home/ec2-user/mmo/mmo_tool/bananav2 "
+FILE="banana.js"
+OUTPUT_FILE="/home/ec2-user/mmo/mmo_tool/bananav2/nohup.out"
+INPUT=$1
+
+if [ -d "$DIRECTORY" ]; then
+  # Di chuyển vào thư mục chứa file JavaScript
+  cd "$DIRECTORY" || { echo "Không thể di chuyển vào thư mục $DIRECTORY"; exit 1; }
+
+  # Kiểm tra xem file JavaScript có tồn tại không
+  if [ -f "$FILE" ]; then
+    # Chạy file JavaScript bằng Node.js với dữ liệu đầu vào từ biến INPUT, ghi output vào nohup.out
+    echo "$INPUT" | nohup node "$FILE" > "$OUTPUT_FILE" 2>&1 &
+    echo "Chạy file JavaScript trong nền, output được ghi vào $OUTPUT_FILE"
+  else
+    echo "File $FILE không tồn tại trong thư mục $DIRECTORY"
+    exit 1
+  fi
+else
+  echo "Thư mục $DIRECTORY không tồn tại"
+  exit 1
+fi
--- timeFarm/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-VAR=$1
-read VAR
-echo "My value is $VAR"
-
